c4f7160325e64ddb798c7346cc547c159097a1db
Juan P. Montecinos <201400076@est.umss.edu>
2021-04-26 20:02:57 -0400

    Cambios consultas SQL y envio de solicutudes a tabal

--- modelo/conexionPablo.php
+++ modelo/conexionPablo.php
@@ -1,12 +1,16 @@
 <?php
-    require('configuracion.php');
-    try{
-        $conexion=new PDO('mysql:=host=localhost; dbname=sistema_de_cotizaciones','root','');
-        $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        $conexion->exec("SET CHARACTER SET UTF8");
-    }
-    catch(Exception $e){
-        die("error") . $e->getMEssage();
-        echo "error!!" . $e->getLine();
+    class Conectar{
+        public static function conexion(){
+            try{
+                $opciones= [PDO::ATTR_CASE=> PDO::CASE_LOWER,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_ORACLE_NULLS=>PDO::NULL_EMPTY_STRING, PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_OBJ];
+                $conexion=new PDO('mysql:host=localhost;dbname=sistema_de_cotizaciones',"root","",$opciones);
+                $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                $conexion->exec("SET CHARACTER SET UTF8");
+            }catch(Exception $e){
+                die("error") . $e->getMessage();
+                echo "error !!" . $e->geLine();
+            }
+            return $conexion;
+        }
     }
 ?>
