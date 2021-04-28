3aefb665a918577ffc876a522a6c57525478741d
carlosgrageda <carlosgrageda55@gmail.com>
2021-04-27 22:32:43 -0400

    prueba

--- vista/vista_solicitudes_aceptadas.php
+++ vista/vista_solicitudes_aceptadas.php
@@ -3,16 +3,12 @@
 
 ?>
 <div class="container-fluid">
-
-
 	<div class="row">
 		<div class="col-md-12">
 			<div class="card">
 				<div class="card-body">
 					<h4 class="card-title">Solicitudes Aceptadas</h4>
-					
 					<div class="row">
-
 						<div id="resultados" class="col-sm-12 "></div>
 						<div class="outer_div" style="width:100%">
 							<div class="table-responsive">
