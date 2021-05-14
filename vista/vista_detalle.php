c1408ef24b3d3b2d62551a49c0227c6046dc6852
marcoescalera <marcoescalera2017@gmail.com>
2021-05-13 22:53:44 -0400

    Implementando Sweetalert2 H10-H12

--- vista/vista_detalle.php
+++ vista/vista_detalle.php
@@ -4,8 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../librerias/js/sweetalert2.all.min.js"></script>
+    <script src="../librerias/js/jquery-3.6.0.js"></script>
     <title>Document</title>
     <link rel="stylesheet" href="css/estilosSolicitud.css">
+
+    <style>
+         .botones{
+            width: auto;
+            margin-top: 100px;
+            text-align: center;
+        }
+        
+        button{
+            display: inline-block;
+        }
+    </style>
+
 </head>
 <body>
 <?php
@@ -55,5 +70,56 @@
         <textarea name="justificacion" id="justificacion" cols="30" rows="10"><?php echo $just?></textarea>
     </div>
 </form>
+
+<!-- MARCO  -->
+
+<div class="botones">
+    <button id="botonAceptar">Aceptar</button>
+    <button id="botonRechazar">Rechazar</button>
+</div>
+
+<script>
+        $('#botonAceptar').on('click', function(){
+            Swal.fire({
+                title: 'Esta seguro de aceptar esta Solicitud?',
+                text: "",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'SI',
+                cancelButtonText: 'NO'
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire('SOLICITUD ACEPTADA!','','success')
+                }else{
+                    Swal.fire('AUTORIZACION CANCELADA!','Se cancelo la autorizacion de la Solicitud','info')
+                }
+            })
+        })
+
+        $('#botonRechazar').on('click', function(){
+            Swal.fire({
+                input: 'textarea',
+                inputLabel: 'Justificacion de Rechazo de Solicitud',
+                inputPlaceholder: 'Ingrese los motivos por el cual se rechazo la solicitud de Pedido...',
+                inputAttributes: {
+                    'aria-label': 'Type your message here'
+                },
+                showCancelButton: true
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire('SOLICITUD RECHAZADA!','Se registro la justificacion para el rechazo de la solicitud','success')
+                }else{
+                    Swal.fire('RECHAZO DE SOLICITUD CANCELADA!','Se cancelo el rechazo de la Solicitud','info')
+                }
+            })
+
+                if (text) {
+                Swal.fire(text)
+                }
+        })   
+    </script>
+
 </body>
 </html>
