ad5595037f5f1d0fa7aab2af4ae116a0674065a4
marcoescalera <marcoescalera2017@gmail.com>
2021-06-08 01:17:16 -0400

    insercion de los usuarios cotizadores en la BD

--- vista/enviarCorreos.php
+++ vista/enviarCorreos.php
@@ -2,9 +2,14 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+require '../configuraciones/conexion.php';
+
 require '../librerias/phpMailer/Exception.php';
 require '../librerias/phpMailer/PHPMailer.php';
 require '../librerias/phpMailer/SMTP.php';
+
+$conn = new Conexion();
+$estadoconexion = $conn->getConn();
 
 if(!empty($_POST)){
     if(empty($_POST["marcar"])){
@@ -13,15 +18,25 @@ if(!empty($_POST)){
         $remitente = $_POST["remitente"];
         $asunto = $_POST["asunto"];
         $descripcion = $_POST["descripcion"];
+        $listaCorreos = obtenerCorreos();
+        $listaIds = obtenerIds();
+        foreach($listaCorreos as $elemento){
+            echo $elemento;
+        }
+        foreach($listaIds as $elementos){
+            echo $elementos;
+        }
+        
         foreach($_POST["marcar"] as $correo_marcado){
             $correo = trim($correo_marcado, '/');
-            enviarCorreos($remitente, $asunto, $descripcion, $correo);
+            $idCorreoActual = obtenerIdEmpresa($correo, $listaCorreos, $listaIds);
+            enviarCorreos($remitente, $asunto, $descripcion, $correo, $idCorreoActual);
         }
         echo '<script language="javascript">window.location.href="../vista/correosEnviados.php?marcado=1";</script>';
     }
 }
 
-function enviarCorreos($remitente, $asunto, $descripcion, $correo){
+function enviarCorreos($remitente, $asunto, $descripcion, $correo, $idCorreoActual){
     $mail = new PHPMailer(true);
     $mail->SMTPOptions = array(
     'ssl' => array(
@@ -49,6 +64,13 @@ function enviarCorreos($remitente, $asunto, $descripcion, $correo){
         $mail->addAddress($correo); //<--Enviar a este correo
         $user = generarUsername();
         $pass = generarPassword();
+
+        while(usuarioExiste($user)){
+            $user = generarUsername();
+        }
+
+        registraUsuarioTemporal($user, $pass, $idCorreoActual, 0);
+
         //Archivos adjuntos
         $mail->addAttachment('../archivos/Solicitud_de_cotizacion_Cod25.pdf');    //Optional name
         $detalles = "<br /><br />Para realizar su cotización puede hacerlo de dos formas posibles, a continuación se detallan las mismas:";
@@ -87,6 +109,89 @@ function generarPassword(){
         $password .= substr($str,rand(0,62),1);
     }
     return $password;
+}
+
+function registraUsuarioTemporal($user, $password, $idEmpresa, $estado){
+    global $estadoconexion;
+    $pass = hashPassword($password);
+    //echo $idEmpresa;
+    $stmt = $estadoconexion->prepare("INSERT INTO usuario_cotizador (user_cotizador, password_cotizador, id_empresa, estado_cotizador) VALUES(?,?,?,?)");
+    $stmt->bind_param("ssii", $user, $pass, $idEmpresa, $estado);
+
+    if($stmt->execute()){
+        return $estadoconexion->insert_id;
+    }else{
+        return 0;
+    }
+}
+
+function obtenerIds(){
+    global $estadoconexion;
+    $resultado = $estadoconexion->query("SELECT id_empresa FROM empresas");
+    //$resultado1 = $estadoconexion->prepare("SELECT correo_empresa FROM empresas");
+    if (!$resultado) {
+        echo 'No se pudo ejecutar la consulta: ' . $estadoconexion->mysql_error();
+        exit;
+    }
+    //$fila=$resultado->fetch_array(MYSQLI_BOTH);
+    //$fila = $estadoconexion->fetch_row($resultado);
+    $fila = array();
+    $i=0;
+    foreach($resultado as $elemento){
+        $fila[$i] = $elemento['id_empresa'];
+        $i++;
+    }
+    return $fila;
+}
+
+function obtenerCorreos(){
+    global $estadoconexion;
+    $resultado1 = $estadoconexion->query("SELECT correo_empresa FROM empresas");
+    if (!$resultado1) {
+        echo 'No se pudo ejecutar la consulta: ' . $estadoconexion->mysql_error();
+        exit;
+    }
+    //$fila=$resultado1->fetch_array(MYSQLI_BOTH);
+    //$fila = $estadoconexion->fetch_row($resultado);
+    $fila = [];
+    $i=0;
+    foreach($resultado1 as $elemento){
+        $fila[$i] = $elemento['correo_empresa'];
+        $i++;
+    }
+    return $fila;
+}
+
+function obtenerIdEmpresa($correo, $listaCorreos, $listaIds){
+    $aux = 0;
+    for($i=0;$i<count($listaCorreos)-1;$i++){
+        if($listaCorreos[$i]==$correo){
+            $aux = $listaIds[$i];
+        }
+    }
+    return $aux;
+}
+
+function usuarioExiste($usuario){
+    global $estadoconexion;
+
+    $stmt = $estadoconexion->prepare("SELECT user_cotizador FROM usuario_cotizador WHERE user_cotizador = ? LIMIT 1");
+    $stmt->bind_param("s", $usuario);
+    $stmt->execute();
+    $stmt->store_result();
+    $num = $stmt->num_rows;
+    $stmt->close();
+    
+    if($num > 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function hashPassword($pass){
+    $hash = password_hash($pass, PASSWORD_DEFAULT);
+    return $hash;
 }
 
 ?>
