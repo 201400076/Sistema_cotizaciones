4d72c3cbfe03ecaff65934c6dee7bfa0068916e2
marcoescalera <marcoescalera2017@gmail.com>
2021-06-25 23:37:54 -0400

    remitente no editable en el envio de correos

--- vista/formularioEnviarCotizaciones.php
+++ vista/formularioEnviarCotizaciones.php
@@ -70,6 +70,8 @@
 </head>
 <body class="fix-header card-no-border">
 <?php
+    session_start();
+    $nombre = $_SESSION['nombreUA'];
     $active = "";
     include_once("layouts/navegacionPendientes.php");
 
@@ -96,25 +98,23 @@
             <div class="row">
                 <div class="col-md-6" >
                     <div style="width: 100%;">
-                        <label for="remitente" style="width: 25%;">Remitente:</label>  
-                        <input name="remitente" id="remitente" type="text" style="width: 50%;" required><br>
+                    <label for="remitente" style="width: 100%">Remitente : <strong style="font-style: italic; font-size: 18px; color: black;"><?php echo $nombre;?></strong></label><br>
                     </div>
                     <div>
-                        <label for="asunto" style="width: 25%;">Asunto:</label>  
-                        <input name="asunto" id="asunto" type="text" style="width: 50%;" required>
+                        <label for="fecInicio" style="width: 100%">Fecha Inicio Cotizaciones     : <strong style="font-style: italic; font-size: 18px; color: black;"><?php echo $fIni; ?></strong></label>  
+                        <label for="fecFin" style="width: 100%">Fecha Fin Cotizaciones     : <strong style="font-style: italic; font-size: 18px; color: black;"><?php echo $fFin;?></strong></label>  
                     </div>
                     <div>
                         <label for="archivo" style="width: 25%;">Archivo adjunto:</label>  
                         <a style="width: 50%;" href="../archivos/cotizacionesIniciales/solicitudCotizacion<?php echo $idSolicitud ?>.pdf" target="_blank">Solicitud_de_cotizacion.pdf</a>
                     </div>
-                    <div>
-                        <label for="fecInicio" style="width: 100%">Fecha Inicio Cotizaciones     : <strong style="font-style: italic; font-size: 18px; color: black;"><?php echo $fIni; ?></strong></label>  
-                    </div>
                 </div>
                 <div class="col-md-6">
+                    <label for="asunto" style="width: 25%;">Asunto:</label>  
+                    <input name="asunto" id="asunto" type="text" style="width: 70%;" required>
                     <label for="descripcion" style="width: 25%;">Descripción:</label> 
                     <textarea name="descripcion" id="descripcion" style="width: 70%;"  cols="50%" rows="3" placeholder="Escriba el cuerpo del correo..." required></textarea> 
-                    <label for="fecFin" style="width: 100%">Fecha Fin Cotizaciones     : <strong style="font-style: italic; font-size: 18px; color: black;"><?php echo $fFin;?></strong></label>  
+                    
                 </div>
 
             </div>
