f9ac33c0a211a1c5942de23b37eef1f6cd17ac85
marcoescalera <marcoescalera2017@gmail.com>
2021-07-08 17:28:45 -0400

    solicitudes expiradas ya no se envian cotizaciones

--- vista/vista_solicitudes_aceptadas.php
+++ vista/vista_solicitudes_aceptadas.php
@@ -1,7 +1,5 @@
 <?php
 	include('layouts/navAdministracion.php');
-	//$active = "";
-	//include_once("layouts/navegacionPendientes.php");
 ?>
 <div class="container-fluid">
 <h2 class="card-title" style="text-align: center;"><strong>SOLICITUDES ACEPTADAS</strong></h2>
@@ -21,17 +19,10 @@
 											<th>Fecha-Recepcion</th>
 											<th>Fecha-Aceptado</th>
 											<th>Solicitante</th>
-											<!-- <th>Detalle</th>
-											<th>Departamento</th>
-											<th>Unidad</th> -->
 											<th>Unidad</th>
 											<th>Estado</th>
 											<th>Accion</th>
 											<th>Cotizacion Pdf</th>
-											
-											
-											<!-- <th class="text-right">Acciones</th> -->
-
 										</tr>
 
 										<?php
@@ -54,18 +45,6 @@
 
 											<td><?php echo $valor[$i]['nombre_gasto']?></td>
 
-											<!-- <td>
-												Compra de Sillas<br>
-												<div class="text-center">
- 													<a href=""> <img src="../recursos/imagenes/pdf.png" class="rounded" alt="chania" width="25" heigth="25"></a>
-												</div>
-											</td>
-											<td>
-												Inf - Sistemas
-											</td>
-											
-											<td>MEMI</td> -->
-
 											<td>
 												<span class="label label-success"><?php echo $valor[$i]['estado']?></span>
 
@@ -81,21 +60,6 @@
 													<input style="padding: 0; margin-top: 0;" type="image" src="../recursos/imagenes/iconoImprimir.png" name="imprimir" alt="imprimir" width="40px"/>
 												</a>
 											</td>
-											
-
-											<!-- <td class="text-right">
-												<div class="btn-group dropleft">
-													<button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-														Acciones
-													</button>
-													<div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 37px, 0px); top: 0px; left: 0px; will-change: transform;">
-														<a class="dropdown-item" href="editar_cotizacion.php?id=230" title="Editar cotización"><i class="fa fa-edit"></i> Editar</a>
-														<a class="dropdown-item" href="#" title="Imprimir cotización" onclick="descargar('303');"><i class="fa fa-print"></i> Imprimir</a>
-														<a class="dropdown-item" href="#" title="Enviar cotización" data-toggle="modal" data-target="#myModal" data-number="303" data-email="usuario@gmail.com"><i class="fa fa-envelope"></i> Enviar Email</a>
-														<a class="dropdown-item" href="#" title="Borrar cotización" onclick="eliminar('230')"><i class="fa fa-trash"></i> Eliminar</a>
-													</div>
-												</div>
-											</td> -->
 
 										</tr>
 
@@ -110,15 +74,7 @@
 										
 									</tbody>
 								</table>
-								<!-- <div class="float-right">
-									<nav aria-label="Page navigation example">
-										<ul class="pagination">
-											<li class="disabled page-item"><span><a class="page-link">‹ Siguiente</a></span></li>
-											<li class="page-item active"><a class="page-link">1</a></li>
-											<li class="page-item"><span><a class="page-link">Anterior ›</a></span></li>
-										</ul>
-									</nav>
-								</div> -->
+
 							</div>
 						</div>
 					</div>
@@ -126,14 +82,8 @@
 			</div>
 		</div>
 	</div>
-
-
-
-
-
 </div>
 
 <?php
 	include('../vista/layouts/piePagina.php');
-	//include_once("layouts/footer.php");
 ?>
